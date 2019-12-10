783c502598f71f126adffe52701b3489b9f7659a
rysa03 <abdrmvv03@gmail.com>
2019-12-10 20:40:23 +0600

    task6_part2

---
+++ part2-task6.py
@@ -0,0 +1,14 @@
+#Given an integer. Print its tens digit.
+# integer = int(input())
+# digit = float(integer)
+# print(digit)
+
+# a=['Jane','Michael','Make']
+# for q in enumerate(a):
+#     print(q)
+# for w,q in enumerate(a,10):
+#     print(w,q)
+
+a = list(range(1, 6))
+for z in enumerate(a):
+print(z[0], z[1])
